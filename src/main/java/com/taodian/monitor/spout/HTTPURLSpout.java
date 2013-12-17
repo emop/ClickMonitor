d5739b0e4f7fbe8c5db1eb8f31e3188e9900311d
wudalong <wudalong@gmail.com>
2013-12-17 11:08:59 +0800

    增加从HTTPURL读取日志的功能。

--- src/main/java/com/taodian/monitor/spout/HTTPURLSpout.java
+++ src/main/java/com/taodian/monitor/spout/HTTPURLSpout.java
@@ -1,24 +1,58 @@
 package com.taodian.monitor.spout;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.taodian.monitor.core.ClickMonitor;
 import com.taodian.monitor.storm.DataCell;
 import com.taodian.monitor.storm.DataSpout;
 import com.taodian.monitor.storm.TopologyContext;
 
 public class HTTPURLSpout implements DataSpout {
+	protected Log log = LogFactory.getLog("lm.mointor.spout");  
+	protected URL logGate = null;
+	private BufferedReader reader = null;
 	
 	public HTTPURLSpout(){
 		
 	}
 	
 	public boolean connect(String url){
-		
-		return false;
+		try{
+			logGate = new URL(url);			
+		}catch(Exception e){
+			return false;
+		}		
+		return openLogUrl();
 	}
 	
 	@Override
 	public DataCell nextDataCell() {
-		// TODO Auto-generated method stub
-		return null;
+		String line = null;
+		DataCell d = null;
+		try {
+			line = reader.readLine();
+			log.debug("log:" + line);
+			if(line != null){
+				d = new DataCell();
+				d.set(ClickMonitor.DATA_RAW, line.trim());
+			}else {
+				openLogUrl();
+				//reader.close();
+				//reader = null;
+			}
+		} catch (IOException e) {
+			log.error(e, e);
+			openLogUrl();
+		}
+
+		return d;
 	}
 
 	@Override
@@ -30,10 +64,47 @@ public class HTTPURLSpout implements DataSpout {
 	@Override
 	public boolean isClosed() {
 		// TODO Auto-generated method stub
-		return false;
+		return reader == null;
 	}
 
-
+	
+	private boolean openLogUrl(){
+		if(logGate == null){
+			return false;
+		}
+		if(reader != null){
+			try {
+				reader.close();
+			} catch (IOException e) {
+			}
+			reader = null;
+		}
+		
+		HttpURLConnection connection = null;
+		
+		log.debug("connecting to " + logGate.toString());
+		try {
+			connection = (HttpURLConnection )logGate.openConnection();
+			connection.setReadTimeout(1000 * 60 * 30);
+			connection.setConnectTimeout(1000 * 5);
+			connection.setRequestMethod("POST");
+			connection.setDoInput(true);
+			connection.connect();
+			reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf8"));
+			//if(reader.)
+			String c = reader.readLine();
+			if(c.trim().startsWith("CONNECTED")){
+				return true;
+			}else {
+				log.info("Failed to connect to click gate, msg:" + c);
+				return false;
+			}
+		} catch (IOException e) {
+			log.error(e.toString(), e);
+		}
+		
+		return false;
+	}
 
 
 }
