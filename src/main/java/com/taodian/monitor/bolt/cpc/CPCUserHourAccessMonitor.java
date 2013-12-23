fa24858a69891587dcc12d813b03184dcc816584
lili <452664116@qq.com>
2013-12-23 17:55:56 +0800

    Update CPCUserHourAccessMonitor.java

--- src/main/java/com/taodian/monitor/bolt/cpc/CPCUserHourAccessMonitor.java
+++ src/main/java/com/taodian/monitor/bolt/cpc/CPCUserHourAccessMonitor.java
@@ -45,7 +45,7 @@ public class CPCUserHourAccessMonitor extends BaseClickMonitor{
 		String outKey = "";
 		outKey = String.format("user_%s_%s", obj.userId, day);
 
-		String innerKey = time+"_"+obj.deviceName;
+		String innerKey = "user_"+obj.userId+"_"+time+"_"+obj.deviceName;
 		t.incr(innerKey);
 		
 		t.lpush(outKey, innerKey);
