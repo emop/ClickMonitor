be037dadb9690a1fe6659518d5d25d95d3aa9b3e
molin0827 <kgd8023yxq@gmail.com>
2013-12-24 17:28:28 +0800

    测试用例中使用python来调用redis
    测试用例中使用python来调用redis

---
+++ atest/lib/RunRedisCommand.py
@@ -0,0 +1,26 @@
+#-*- coding: utf8 -*-
+import redis
+
+class RunRedisCommand:
+	def __init__(self, host="127.0.0.1", port=6379):
+		self.cons = dict()
+		self.host = host
+		self.port = int(port)
+		#self.f = redis.StrictRedis(host=host, port=int(port) )
+	def get_data(self, key, db=0):
+		f = self._get_con(db)
+		val = f.get(key)
+		return val
+
+	def cleanup_db_data(self, db=0):
+		f = self._get_con(db)
+		f.flushdb()
+		
+	
+	def _get_con(self, db):
+		db = int(db)
+		f = self.cons.get(db)
+		if f is None:
+			f = redis.StrictRedis(host=self.host, port=self.port, db=int(db) )
+			self.cons[db] = f
+		return f
